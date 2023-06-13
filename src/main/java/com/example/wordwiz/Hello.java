3ec4f6967d3909f3c8e300f3a5b27b9b5ca94d71
Emanuele <egalli64@gmail.com>
2023-06-13 21:51:44 +0200

    DataSource example

--- src/main/java/com/example/wordwiz/Hello.java
+++ src/main/java/com/example/wordwiz/Hello.java
@@ -1,17 +1,34 @@
 package com.example.wordwiz;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
 
 @SuppressWarnings("serial")
 @WebServlet("/hello")
 public class Hello extends HttpServlet {
+    @Resource(name = "jdbc/wordwiz")
+    private DataSource ds;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.getWriter().append("Hello!");
+        try (Connection connection = ds.getConnection(); //
+                Statement stmt = connection.createStatement(); //
+                ResultSet rs = stmt.executeQuery("select count(*) from users")) {
+            rs.next();
+            response.getWriter().append(rs.getInt(1) + " records in users table");
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
