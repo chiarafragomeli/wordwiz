623513f379df19acec42b5f68e78b84405c19523
Claudia <claudia.m.97@hotmail.com>
2023-06-27 11:56:29 +0200

    admin info added

--- src/main/java/com/example/wordwiz/Login.java
+++ src/main/java/com/example/wordwiz/Login.java
@@ -8,6 +8,7 @@ import com.example.wordwiz.dao.User;
 import com.example.wordwiz.svc.LoginSvc;
 
 import jakarta.annotation.Resource;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -27,6 +28,20 @@ public class Login extends HttpServlet {
 			throws ServletException, IOException {
 		String username = request.getParameter("user");
 		String password = request.getParameter("password");
+		
+//		check for superUser
+		ServletContext sc = getServletContext();
+		String admin = (String) sc.getAttribute("superUser");
+		String adminPassword = (String) sc.getAttribute("superUserPassword");
+		if (username.equals(admin) && password.equals(adminPassword)) {
+		    User user = new User(0, admin, (String) sc.getAttribute("superUserEmail"));
+		    HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+//            inserire nei puntini la pagina dell'amministratore
+            request.getRequestDispatcher("...").forward(request, response);
+            return;
+		}
+		
 		LoginSvc svc = new LoginSvc(ds);
 		User user = svc.getUser(username, password);
 		if (user != null) {
