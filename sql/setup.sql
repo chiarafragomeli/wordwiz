c205ed89327bb3c466a0291779f0ab13b09f378d
Claudia <claudia.m.97@hotmail.com>
2023-06-28 16:34:01 +0200

    messages

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,6 @@
 drop table if exists entries;
 drop table if exists users_texts;
+drop table if exists messages;
 drop table if exists users;
 drop table if exists texts;
 drop table if exists gramm_class;
@@ -28,6 +29,7 @@ create table users(
 
 
 insert into users (username, email, password) values 
+  ('admin', 'admin@wordwiz.com', 'password'),
   ('Ari', 'ari@gmail.com', '1234'),
   ('Ri', 'ri@gmail.com', '5678'),
   ('Flora', 'flora@gmail.com', '9101112');
@@ -73,3 +75,21 @@ CREATE table entries (
 	foreign key (class_id) references gramm_class (class_id),
 	foreign key (user_id) references users (user_id)
 	);
+
+  CREATE table messages (
+	message_id serial primary key,
+	message varchar (10000) not null,
+  time timestamp not null DEFAULT CURRENT_TIMESTAMP,
+  sender_id integer not null,
+  recipient_id integer not null,
+	foreign key (sender_id) references users (user_id),
+	foreign key (recipient_id) references users (user_id)
+	);
+
+  insert into messages (message, time, sender_id, recipient_id) values 
+ ('login problems', '2023-05-12 12:03:00', 2, 1),
+ ('new entry problems', '2023-05-13 09:40:00', 3, 1);
+
+  insert into messages (message, sender_id, recipient_id) values 
+  ('forgot password', 3, 1),
+  ('I would like to send messages to other users', 4, 1);
